22cfc3acfe60814fe24d76a9a54e2dc23f135227
rakibul-islam2004 <mdrakibulislamandrokib@gmail.com>
2025-12-03 23:11:21 +0600

    lodo create and get all lodo api

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { Pool } from "pg";
 import dotenv from "dotenv";
 import path from "path";
@@ -42,11 +42,17 @@ const initDB = async () => {
 
 initDB();
 
+// logger middleware
+const logger = (req: Request, res: Response, next: NextFunction) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.path}\n`);
+  next();
+};
+
 // parser
 app.use(express.json());
 // app.use(express.urlencoded)
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", logger, (req: Request, res: Response) => {
   res.send("Hello Next level developer!");
 });
 
@@ -172,6 +178,55 @@ app.delete("/users/:id", async (req: Request, res: Response) => {
       message: err.message,
     });
   }
+});
+
+// create todo
+app.post("/todos", async (req: Request, res: Response) => {
+  const { user_id, title } = req.body;
+
+  try {
+    const result = await pool.query(
+      `INSERT INTO todos(user_id,title) VALUES($1,$2) RETURNING *`,
+      [user_id, title]
+    );
+    res.status(201).json({
+      success: true,
+      message: "To Do created",
+      data: result.rows[0],
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+// Get all todos
+app.get("/todos", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM todos`);
+
+    res.status(200).json({
+      success: true,
+      message: "Todos retrieved successfully",
+      data: result.rows,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: "err.message",
+      details: err,
+    });
+  }
+});
+
+app.use((req: Request, res: Response) => {
+  res.status(404).json({
+    success: false,
+    message: "Route is not found. TrY AgAIn",
+    path: req.path,
+  });
 });
 
 app.listen(port, () => {
